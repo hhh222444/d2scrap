9b053ed9f6ce65eceb50f1cde605e69332ea31f7
hh <hung@abc.com>
2023-08-15 21:33:22 +0800

    !Adding hero json

--- scrap_hero.py
+++ scrap_hero.py
@@ -1,10 +1,36 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 ua = UserAgent()
 headers = {'User-Agent': ua.chrome}
+hero_list_json_file = "hero_list.json"
 
-hero_url = "https://www.dota2.com/datafeed/herolist?language=english"
-hero_response = requests.get(hero_url, headers=headers)
+def fetch_hero_list():
+    hero_list_url = "https://www.dota2.com/datafeed/herolist?language=english"
+    hero_list_response = requests.get(hero_list_url, headers=headers).json()
+    hero_list = hero_list_response['result']['data']['heroes']
+    with open(hero_list_json_file, 'w') as f:
+        json.dump(hero_list, f)
+
+
+def fetch_hero(id):
+    hero_url = "https://www.dota2.com/datafeed/herodata?language=english&hero_id=" + str(id)
+    print("fetching hero " + str(id) + " from " + hero_url)
+    hero_response = requests.get(hero_url, headers=headers).json()
+    hero_list = hero_response['result']['data']['heroes'][0]
+    with open('heroes\hero_' + str(id) + '.json', 'w') as f:
+        json.dump(hero_list, f)
+
+
+def fetch_heroes():
+    with open(hero_list_json_file, 'r') as f:
+        hero_list = json.load(f)
+    for hero in hero_list:
+        fetch_hero(hero['id'])
+
+# fetch_hero_list()
+
+fetch_heroes()
 
